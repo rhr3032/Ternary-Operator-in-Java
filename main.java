7049d295ced2248f90c68b881751a4cd4f9b871e
Raisul Hasan Rafi <raisulhasanrafi3032@gmail.com>
2022-01-19 10:33:58 -0800

    committed all file

---
+++ main.java
@@ -0,0 +1,11 @@
+public static void main(String[] args) {
+
+        String languageOne = "Python"
+        String languageTwo = "Django";
+
+        String frameworkLanguage = 
+            (languageOne =/= languageTwo ? "Python" : "Django");
+
+        
+    System.out.println(frameworkLanguage);
+}
